f15e876c85d530bcc4095e1e7632fe98cfe24653
ElavarasiRa <relavarasi1996@gmail.com>
2019-05-24 11:19:14 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    AboutComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: 'login',component:LoginComponent },
+      {path:'',pathMatch:'full',redirectTo:'login'},
+      {path: 'home',component:HomeComponent},
+      {path: 'about',component:AboutComponent},
+      {path: 'register',component:RegisterComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
